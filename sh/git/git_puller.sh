79df800daa452e81c0cad499160d98ef80134e9c
sergiocy <sergiocordoba82@gmail.com>
2020-01-01 14:22:09 +0100

    working on git_puller utility

--- sh/git/git_puller.sh
+++ sh/git/git_puller.sh
@@ -1,9 +1,12 @@
 
 # ...get input args...
+# TODO: improve parsing... function to parsing arguments
 REPO=$1
 PATH_REL_LIB=$2
+PUSH_REPO=$3
+COMMIT_MESSAGE=$4
 
-# ...parameters definition...
+# ...(global) parameters definition...
 PATH_ROOT_PROJECTS="/home/sergio/sc-sync/projects/"
 BRANCH="master"
 BRANCH_LIB="master"
@@ -38,4 +41,20 @@ pwd
 # ...return to repo path...
 cd $PATH_ROOT_REPO
 pwd
+
+if [ "$3" == "push_repo" ] 
+then
+	echo "----- PUSH REPO -----"
+	git status
+	git add .
+	git status
+	if [ "$4" != "" ]
+	then
+		git commit -m $4
+		git push origin $BRANCH 
+	else
+		git commit -m "pushing from sh utility"
+		git push origin $BRANCH
+	fi
+fi
 
